92832e4f2adbd4d08bd735a7e776c49a9c5a13ae
smrtghost <112229707+smrtghost@users.noreply.github.com>
2022-11-07 21:51:33 +0530

    Update settings.py

--- teravideos/settings.py
+++ teravideos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,8 +143,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
+django_heroku.settings(locals())
 
 
 # Default primary key field type
