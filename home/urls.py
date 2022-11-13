53c88160c57f77288712e408770dc43074a91c3e
smrtghost <112229707+smrtghost@users.noreply.github.com>
2022-11-13 21:30:44 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -14,4 +15,4 @@ urlpatterns = [
     path('termsConditions', views.termsConditions, name='termsConditions'),
     path('search', views.search, name='search'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
