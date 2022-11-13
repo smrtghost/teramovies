efd0587e754f399b8f5697d60f805d9b9abace16
smrtghost <112229707+smrtghost@users.noreply.github.com>
2022-11-13 21:26:51 +0530

    Update urls.py

--- bengali/urls.py
+++ bengali/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -10,4 +12,4 @@ urlpatterns = [
     path('bengwebseries', views.bengwebseries, name='bengwebseries'),
     
      
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
