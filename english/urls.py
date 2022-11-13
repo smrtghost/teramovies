c67767393f4dd95ea0686d63f869bf0ceeb7b09a
smrtghost <112229707+smrtghost@users.noreply.github.com>
2022-11-13 21:27:58 +0530

    Update urls.py

--- english/urls.py
+++ english/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path('engwebseries', views.engwebseries, name='engwebseries'),
     
      
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
